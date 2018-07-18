cc2ec5b0cf7bfa4808075eaf77f0657c4794c9b0
ramprasathc <32586278+ramprasathc@users.noreply.github.com>
2018-07-17 23:51:06 -0300

    Update module.js

--- module.js
+++ module.js
@@ -1,35 +1,15 @@
-const pg        = require('pg');
-const express   = require('express');
-const app       = express();
+var express = require('express');
 
-const config = {
-    user: 'postgres',
-    database: 'booking_hotel',
-    host:'localhost',
-    password: 'postgres',
-    port: 5432
-};
+var app = express();
 
-// pool takes the object above -config- as parameter
-const pool = new pg.Pool(config);
-
-app.get('/login', (req, res, next) => {
-   pool.connect(function (err, client, done) {
-       if (err) {
-           console.log("Can not connect to the DB" + err);
-       }
-       client.query('SELECT * FROM master_login', function (err, result) {
-            done();
-            if (err) {
-                console.log(err);
-                res.status(400).send(err);
-            }
-
-            res.status(200).send(result.rows);
-       })
-   })
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\'re in reception');
 });
 
-app.listen(4000,'0.0.0.0', function () {
-    console.log('Server is running.. on Port 4000');
+
+app.get('/floor/:floornum/bedroom', function(req, res) {
+    res.render('bedroom.ejs', {floor: req.params.floornum});
 });
+
+app.listen(8080);
