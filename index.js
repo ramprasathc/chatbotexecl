706e7ba5ab718d7f49e0fb6bfe23c1d292379212
ramprasathc <32586278+ramprasathc@users.noreply.github.com>
2018-07-19 15:52:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const config = {
 // pool takes the object above -config- as parameter
 const pool = new pg.Pool(config);
 
-app.post('/login', (req, res, next) => {
+app.get('/login', (req, res, next) => {
      //   if (req.body.queryResult.parameters == "login")
   //  if(true)
      //   {
