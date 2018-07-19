7701b8279f70a9ad2124517373b2f9ad57f638f0
ramprasathc <32586278+ramprasathc@users.noreply.github.com>
2018-07-19 17:15:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const config = {
 const pool = new pg.Pool(config);
 
 app.post('/login', (req, res, next) => {
+    console.log('Start');
     var id = (req.body.responseId);
     if(id== undefined || id.length !=0)
     {
